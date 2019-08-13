e23075bd4b5af1af8a5ea48f6bfc42e0574452c8
Cloe Kouadjo <armelfli50@gmail.com>
2019-08-13 01:58:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(array)
+  string = "The line is currently: "
+ if array == []
+   puts "The line is currently empty."
+ else
+    array.each_with_index do |el,index|
+      string << "#{index + 1}. #{el} "
+    end
+    puts string
+ end
+ 
+end 
+
+def take_a_number(array, new_customer) 
+  array << new_customer
+  puts "#{new_customer} with the number #{array.length + 1}"
+end
+
+def now_serving(array)
+  line(array)
+  
+end
